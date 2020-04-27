8c23c701b75736b63f0725c7a8c775874f691179
kirriri <ararinn@outlook.com>
2020-04-28 01:21:59 +0800

    input dev 0427

--- src/components/Button/button.stories.tsx
+++ src/components/Button/button.stories.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
-import { withInfo } from '@storybook/addon-info'
 import Button from './button'
 
 const defaultButton = () => (
@@ -9,10 +8,10 @@ const defaultButton = () => (
 )
 
 const buttonWithSize = () => (
-  <>
+  <div>
     <Button size="lg"> large button </Button>
     <Button size="sm"> small button </Button>
-  </>
+  </div>
 )
 
 const buttonWithType = () => (
@@ -27,4 +26,4 @@ storiesOf('Button component', module)
   // .addDecorator(withInfo)
   .add('Button', defaultButton)
   .add('不同尺寸的 Button', buttonWithSize)
-  .add('不同类型的 Button', buttonWithType)
+  // .add('不同类型的 Button', buttonWithType)
